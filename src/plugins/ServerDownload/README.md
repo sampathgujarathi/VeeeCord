f2eb273ad556a9eec0d5ebb5e3896dda2007fc6a
Sampath Gujarathi <gujarathisampath@gmail.com>
2024-03-09 08:36:40 +0530

    Added few trusted 3rd party plugins

---
+++ src/plugins/ServerDownload/README.md
@@ -0,0 +1,7 @@
+# ServerDownload
+Adds multiple context menu options for downloading a server's data. 
+Uh, don't @ me if the code is bad; I don't care. It should be functional. DM me on Discord if you have any ideas on other things to download. Credits to Lumap for the [emoji download snippet](https://discord.com/channels/1015060230222131221/1028106792737185842/1141154160650240050).
+
+## Preview
+
+![Preview](https://github.com/cheesesamwich/ServerDownload/blob/main/Discord_oSBOubCRv2.gif?raw=true)
---
+++ src/plugins/WriteUpperCase/index.ts
@@ -0,0 +1,55 @@
+import { addPreSendListener, removePreSendListener } from "@api/MessageEvents";
+import definePlugin, { OptionType } from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import { Settings } from "@api/Settings";
+
+const settings = definePluginSettings(
+    {
+        blockedWords: {
+            type: OptionType.STRING,
+            description: "Strings not to capitilise (seperate with a comma)",
+            default: "http, ok"
+        },
+}
+)
+
+export default definePlugin({
+    name: `WriteUpperCase`,
+    description: "Changes the first Letter of each Sentence in Message Inputs to Uppercase (Port from bd)",
+    authors: [
+        {
+            id: 843924873406840842n,
+            name: "Samwich",
+        },
+    ],
+    patches: [],
+
+    start()
+     {
+        this.preSend = addPreSendListener(async (_, message) => {
+            message.content = textProcessing(message.content);
+        });
+    },
+    stop() 
+    {
+        this.preSend = removePreSendListener(async (_, message) => {
+            message.content = textProcessing(message.content);
+        });
+    },
+    settings
+});
+
+
+// this is probably really badly coded im so fucking sorry :skull:
+function textProcessing(textInput: string): string {
+    let sentences = textInput.split(/(?<=\w\.)\s/);
+    let blockedWordsArray: string[] = Settings.plugins.WriteUpperCase.blockedWords.split(", ");
+    
+    return sentences.map(element => {
+        if (!blockedWordsArray.some(word => element.toLowerCase().startsWith(word.toLocaleLowerCase()))) {
+            return element.charAt(0).toUpperCase() + element.slice(1);
+        } else {
+            return element;
+        }
+    }).join(" ");
+}
